c1f84c6cd20c91f9ca012133e9bbc7034a959e13
ismail.palali.pp@gmail.com <ismail.palali.pp@gmail.com>
2022-05-16 19:36:34 +0300

    add searchBar & global constants

---
+++ SearchApp/Network/GlobalConstants.swift
@@ -0,0 +1,18 @@
+//
+//  GlobalConstants.swift
+//  SearchApp
+//
+//  Created by ismail palali on 16.05.2022.
+//
+
+import Foundation
+import UIKit
+
+struct GlobalConstants{
+    static let COLOR_LIGHT_BLACK :UIColor = UIColor(red: 80/255, green: 90/255, blue: 110/255, alpha: 1.0)
+    static let COLOR_LIGHT_GRAY :UIColor = UIColor(red: 211/255, green: 211/255, blue: 211/255, alpha: 1.0)
+    static let COLOR_NAV_COLOR :UIColor = UIColor(red: 0.898, green: 0, blue: 0, alpha: 1.0)
+    static let COLOR_BORDER_BLUE :UIColor = UIColor(red: 0.898, green: 0, blue: 0, alpha: 1.0)
+    
+
+}
