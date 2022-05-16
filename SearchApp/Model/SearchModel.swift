8995c764fa5d87aa92dbbd8bed84ba9177d1d478
ismail.palali.pp@gmail.com <ismail.palali.pp@gmail.com>
2022-05-16 19:26:03 +0300

    add model classes

---
+++ SearchApp/Model/SearchModel.swift
@@ -0,0 +1,33 @@
+//
+//  SearchModel.swift
+//  SearchApp
+//
+//  Created by ismail palali on 16.05.2022.
+//
+
+import Foundation
+
+struct ContentModel: Codable {
+    var wrapperType: String?
+    var kind: String?
+    var collectionName: String?
+    var artworkUrl30: String?
+    var artworkUrl60: String?
+    var artworkUrl100: String?
+    var explicitness: String?
+    var trackName: String?
+    var artistName: String?
+    var previewUrl: String?
+    var collectionPrice: Double?
+    var trackPrice: Double?
+    var releaseDate: String?
+    var country: String?
+    var currency: String?
+    var shortDescription: String?
+    var longDescription: String?
+    var primaryGenreName: String?
+    var releaseNotes: String?
+    var description: String?
+    var ratingIndex: String?
+    var showTerm: String?
+}
---
+++ SearchApp/Model/SectionModel.swift
@@ -0,0 +1,13 @@
+//
+//  SectionModel.swift
+//  SearchApp
+//
+//  Created by ismail palali on 16.05.2022.
+//
+
+import Foundation
+
+struct SegmentModel{
+    let segmentTitle :String
+    let contentList: [ContentModel]
+}
